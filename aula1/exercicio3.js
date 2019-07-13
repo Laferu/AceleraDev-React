c45b82fcfc51b06862816d07f7ea5e4b8bd9586b
Laferu <laferu@users.noreply.github.com>
2019-07-13 16:36:42 -0300

    melhorando o código do exercício 3

--- aula1/exercicio3.js
+++ aula1/exercicio3.js
@@ -1,7 +1,7 @@
 const sum = (function() {
   "use strict";
-  return function sum(x, y, ...z) {
-    const args = [ x, y, ...z ];
+  return function sum(...z) {
+    const args = [...z ];
     return args.reduce((a, b) => a + b, 0);
   };
  })();
