161ad2e6b38a3938370436412ff81c1d8271431e
Laferu <laferu@users.noreply.github.com>
2019-07-20 14:05:09 -0300

    Reaproveitamento de componentes

--- aula2/facebook/src/App.js
+++ aula2/facebook/src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+
+function Title(){
+  return(
+    <h1><Text text="Meu título mudado" /></h1>
+  )
+}
+
+function Content(){
+  return(
+  <div>
+    <Text text="Meu Texto" />
+  </div>
+  )
+}
+
+function Text(props){
+  return(
+    <span className="text">
+      {props.text}
+    </span>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Title />
+      <Content />
     </div>
   );
 }
